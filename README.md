a8b81415b57f705a9d34b43cea65b78ae16966f5
Myroslav Kiurchev <kiurchv@gmail.com>
2016-12-16 03:01:02 +0200

    Add deprecation notice. Close #1 (#2)

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 [Babel](https://babeljs.io/) plugin that transforms ES2015 System.import into CommonJS require.
 
+## Deprecation notice
+
+** Since [import() proposal](https://github.com/tc39/proposal-dynamic-import) was accepted, `System.import` function now is obsolete and this plugin is not recommended to use in new projects. Please use  `import()` function with [babel-plugin-transform-import-commonjs](https://www.npmjs.com/package/babel-plugin-transform-import-commonjs) instead. **
+
+
 ## How this works
 
 This plugin transforms
--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ let buildPromise = template(`
   }.bind(this));
 `);
 
+let deprecationReported = false;
+
 export default function () {
+  if (!deprecationReported) {
+    console.warn(
+`babel-plugin-transform-system-import-commonjs: Using System.import is deprecated.
+Since import() proposal was accepted, System.import function now is obsolete and this plugin is not recommended to use in new projects.
+Please use import() function with https://www.npmjs.com/package/babel-plugin-transform-import-commonjs instead.`
+    );
+
+    deprecationReported = true;
+  }
+
   return {
     visitor: {
       CallExpression: function (path) {
